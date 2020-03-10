0524e99cae23db407a922357ddbdd4e9657b2b14
nodormu <60243248+nodormu@users.noreply.github.com>
2020-03-09 20:08:59 -0500

    Add files via upload

---
+++ Kotlin-111-Incorporating-Material-into-codebase/kotlin/shipping/app/src/main/java/com/google/codelabs/mdc/kotlin/shipping/ShippingInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.google.codelabs.mdc.kotlin.shipping
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ShippingInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.shipping_info_activity)
+    }
+}
